038d182b663db7e9f860071bcab7c19650ab04c9
haniya1604 <haniyazaidi16@gmail.com>
2023-10-30 11:55:41 -0700

    initial commit

---
+++ Desktop/LMS/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    database: "LMS",
+    username: "postgres",
+    password: "bubbletea16",
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+  },
+  jwt: "JzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpva",
+};
---
+++ Desktop/LMS/controller/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  userController: require("./users/userController"),
+  studentController: require("./users/studentController"),
+  courseController: require("./users/courseController"),
+  teacherController: require("./users/teacherController"),
+  teacherCourseController: require("./users/teacherCourseController"),
+  authenticationController: require("./common/authenticationController"),
+};
